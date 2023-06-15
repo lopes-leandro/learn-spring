b967b9b37b7835ede5407b9f8f7cfe51dc1de67f
Leandro Lopes <lopesleandro.it@gmail.com>
2023-06-15 17:25:07 -0300

    Addconfig properties access database

---
+++ src/main/java/com/llopes/superheroes/antiHero/entity/AntiHeroEntity.java
@@ -0,0 +1,10 @@
+package com.llopes.superheroes.antiHero.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "anti_hero_entity")
+public class AntiHeroEntity {
+    
+}
