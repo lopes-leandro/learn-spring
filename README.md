4ed1fb78a37ee407078e7f408698d769e8c58716
Leandro Lopes <lopesleandro.it@gmail.com>
2023-06-16 03:15:02 -0300

    Create AntiHero controller

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Simple project to learning Spring Framework
 * IndexOutOfBoundsException: essa exceção ocorre quando acessamos um índice de um array de string ou interável está fora do intervalo;
 * IllegalArgumentException: essa exceção ocorre quando um método foi passado por um argumento ilegal;
 * NullPointerException: esse exceção ocorre quando uma variável é acessada não aponta para nenhum objeto ou nulo;
+
+# Criando Controladores no Spring Framework
+
+* __@RestController__ : é uma forma especializada da anotação __@Controller__, ele já inclui a anotação __@Controller__ e __@ResponseBody__, não sendo necessário especificar a anotação __@ResponseBody__ em cada método.
+
+* __@RequestMapping__ : é uma anotação usada para mapear as solicitações do HyperText Transfer Protocol (HTTP) aos métodos dos controladores Representational State Transfer (REST). É também onde definimos um caminho base para o controlador.
+
+* __@GetMapping__ : mapeia solicitações __HTTP GET__ em um método; a anotação é um atalho para __@RequestMapping(method = RequestMethod.GET)__
+
+* __@PutMapping__ : mapeia solicitações __HTTP PUT__ em um método; a anotação é um atalho para __@RequestMapping(method = RequestMethod.PUT)__
+
+* __@PostMapping__ : mapeia solicitações __HTTP POST__ em um método; a anotação é um atalho para __@RequestMapping(method = RequestMethod.POST)__
+
+* __@DeleteMapping__ : mapeia solicitações __HTTP DELETE__ em um método; a anotação é um atalho para __@RequestMapping(method = RequestMethod.DELETE)__ 
+
+* __@PathVariable__ : essa anotação é usada para obter o valor dos parâmetros do terminal
+
+* __@Valid__ : essa anotação é usada para verificar a validade de um objeto; é comumente usado no corpo da solicitação para verificar se uma solicitação passada é um objeto válido;
---
+++ src/main/java/com/llopes/superheroes/antiHero/controller/AntiHeroController.java
@@ -0,0 +1,79 @@
+package com.llopes.superheroes.antiHero.controller;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.llopes.superheroes.antiHero.dto.AntiHeroDto;
+import com.llopes.superheroes.antiHero.entity.AntiHeroEntity;
+import com.llopes.superheroes.antiHero.service.AntiHeroService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/anti-heroes")
+public class AntiHeroController {
+
+    private final ModelMapper mapper;
+    private final AntiHeroService antiHeroService;
+
+    @GetMapping("/{id}")
+    public AntiHeroDto getAntiHeroById(@PathVariable("id") UUID id) {
+        return convertToDto(antiHeroService.findAntiHeroById(id));
+    }
+
+    @PostMapping
+    public AntiHeroDto postAntiHero(@Valid @RequestBody AntiHeroDto dto) {
+        var entity = convertToEntity(dto);
+        var antiHero = antiHeroService.addAntiHero(entity);
+        return convertToDto(antiHero);
+    }
+
+    @PutMapping("/{id}")
+    public void putAntiHero(@PathVariable("id") UUID id, @Valid @RequestBody AntiHeroDto dto) {
+        if (!id.equals(dto.getId()))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id does not match.");
+        var entity = convertToEntity(dto);
+        antiHeroService.updateAntiHero(id, entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAntiHeroById(@PathVariable("id") UUID id) {
+        antiHeroService.removeAntiHeroById(id);
+    }
+
+    @GetMapping
+    public List<AntiHeroDto> getAntiHeroes() {
+        var stream = StreamSupport
+                .stream(this.antiHeroService.findAllAntiHeroes().spliterator(), false)
+                .collect(Collectors.toList());
+        return stream
+                .stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    private AntiHeroDto convertToDto(AntiHeroEntity entity) {
+        return mapper.map(entity, AntiHeroDto.class);
+    }
+
+    private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
+        return mapper.map(dto, AntiHeroEntity.class);
+    }
+}
