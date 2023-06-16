43302b0a9f3b61e56d48b06a63aa12fde5256222
Leandro Lopes <lopesleandro.it@gmail.com>
2023-06-16 03:53:20 -0300

    Add annotation @Bean in ModelMapperConfig

--- src/main/java/com/llopes/superheroes/antiHero/controller/AntiHeroController.java
+++ src/main/java/com/llopes/superheroes/antiHero/controller/AntiHeroController.java
@@ -2,7 +2,6 @@ package com.llopes.superheroes.antiHero.controller;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
--- src/main/java/com/llopes/superheroes/config/ModelMapperConfig.java
+++ src/main/java/com/llopes/superheroes/config/ModelMapperConfig.java
@@ -2,10 +2,13 @@ package com.llopes.superheroes.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
 
 @Configuration
 public class ModelMapperConfig {
-    
+
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
